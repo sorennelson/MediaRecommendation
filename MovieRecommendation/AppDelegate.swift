a590ef106809dfdce1ee840b83feb326f6d8fde7
Soren Nelson <sorennelson33@gmail.com>
2018-07-17 17:47:12 -0600

    Initial Commit

---
+++ MovieRecommendation/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MovieRecommendation
+//
+//  Created by Soren Nelson on 7/17/18.
+//  Copyright © 2018 SORN. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MovieRecommendation/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MovieRecommendation
+//
+//  Created by Soren Nelson on 7/17/18.
+//  Copyright © 2018 SORN. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
