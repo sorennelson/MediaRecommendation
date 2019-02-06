1e1ccc223f7221c2fa6e04a2b44bb1e267f1bf09
Soren Nelson <sorennelson33@gmail.com>
2019-02-06 12:20:10 -0700

    Revert "Revert "Expansion for Recommendations""
    This reverts commit e37b8a35f0c25501854058dc4186662ebeedcc38.

--- MovieRecommendation/ViewController.swift
+++ MovieRecommendation/ViewController.swift
@@ -12,18 +12,40 @@ class ViewController: NSViewController {
     
     @IBOutlet var ratingsTableView: NSTableView!
     var ratingsDataSource = RatingsTableView()
-    
     @IBOutlet var recommendationTableView: NSTableView!
     var recommendationDataSource = RecommendationTableView()
-    
-
     @IBOutlet var categoriesTableView: NSTableView!
     var categoriesDataSource = CategoriesTableView()
     
+    var expansion = Expansion.notExpanded
+    @IBOutlet var recToViewConstraint: NSLayoutConstraint!
+    @IBOutlet var recToCategoriesConstraint: NSLayoutConstraint!
+    @IBOutlet var ratingsToViewConstraint: NSLayoutConstraint!
+    @IBOutlet var ratingsToCategoriesConstraint: NSLayoutConstraint!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+<<<<<<< Updated upstream
         
+=======
+        setupTableViews()
+        self.view.window?.backgroundColor = NSColor(red: 0.0898, green: 0.0938, blue: 0.0938, alpha: 1)
+
+        DispatchQueue.global(qos: .background).async {
+            print("This is run on the background queue")
+            
+            ParseController.sharedInstance.importAndParseData()
+            var RM = ParseController.sharedInstance.importToMLModel()
+            HypothesisEvaluation.sharedInstance.trainData(iterations: 300, RM: &RM)
+        }
+
+// 0.0008 - too large
+// 0.00079 - works
+//        runTests()
+    }
+    
+    private func setupTableViews() {
+>>>>>>> Stashed changes
         ratingsTableView.dataSource = ratingsDataSource
         ratingsTableView.delegate = ratingsDataSource
         ratingsTableView.backgroundColor = NSColor(red: 0.1205, green: 0.1232, blue: 0.1287, alpha: 1)
@@ -31,12 +53,13 @@ class ViewController: NSViewController {
         
         recommendationTableView.dataSource = recommendationDataSource
         recommendationTableView.delegate = recommendationDataSource
-        recommendationTableView.backgroundColor = NSColor(red: 0.08493, green: 0.08767, blue: 0.09041, alpha: 1)
+        recommendationTableView.backgroundColor = NSColor(red: 0.0898, green: 0.0938, blue: 0.0938, alpha: 1)
         recommendationTableView.reloadData()
         
         categoriesTableView.dataSource = categoriesDataSource
         categoriesTableView.delegate = categoriesDataSource
-        categoriesTableView.backgroundColor = NSColor(red: 0.1506, green: 0.1945, blue: 0.2164, alpha: 1)
+        categoriesTableView.backgroundColor = NSColor(red: 0.152, green: 0.215, blue: 0.246, alpha: 1)
+        // 39 55 63
         categoriesTableView.reloadData()
 
         DispatchQueue.global(qos: .background).async {
@@ -58,6 +81,7 @@ class ViewController: NSViewController {
 // 0.00079 - works
 //        runTests()
     }
+    
     
     private func runTests() {
         let test = Test.sharedInstance
